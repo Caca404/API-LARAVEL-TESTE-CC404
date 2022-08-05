8dd3aef76f2cab5ad75af28cf97c9ae911a7b84d
Caca404 <christia10k@hotmail.com>
2022-08-04 22:32:17 -0300

    :sparkles: feat: api url is working

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\V1\CustomerController;
+use App\Http\Controllers\API\V1\InvoiceController;
+use App\Models\Customer;
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['prefix' => 'V1'], function(){
+    Route::apiResource('customers', CustomerController::class);
+    Route::apiResource('invoices', InvoiceController::class);
 });
