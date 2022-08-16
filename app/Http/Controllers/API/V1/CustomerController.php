c900fe40866881d136bb10e42f55c815bc9b4a4c
Caca404 <christia10k@hotmail.com>
2022-08-16 10:56:02 -0300

    :sparkles: add rules to make a good post request

--- app/Http/Controllers/API/V1/CustomerController.php
+++ app/Http/Controllers/API/V1/CustomerController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API\V1;
 
 use App\Filters\V1\CustomerFilter;
 use App\Models\Customer;
-use App\Http\Requests\StoreCustomerRequest;
+use App\Http\Requests\V1\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\CustomerCollection;
@@ -34,16 +34,6 @@ class CustomerController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \App\Http\Requests\StoreCustomerRequest  $request
@@ -51,7 +41,7 @@ class CustomerController extends Controller
      */
     public function store(StoreCustomerRequest $request)
     {
-        //
+        return new CustomerResource(Customer::create($request->all()));
     }
 
     /**
